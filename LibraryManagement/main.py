a36d2417230f4fa435c83919e372fd71cc557ffa
Chrisin25 <chrising2001@gmail.com>
2024-09-09 16:07:20 +0530

    -

--- LibraryManagement/main.py
+++ LibraryManagement/main.py
@@ -1,41 +1,44 @@
 from LibraryManagement.library_module import Library,LibraryUser
-print("library")
-new_library=Library()
-stop=False
-while not stop:
-    choice = int(input("Enter your choice:1.add book 2.remove book 3.search book 4.display book."))
-    if choice==1:
-        print(new_library.add_book())
-    elif choice==2:
-        book_id = int(input("enter book id:"))
-        print(new_library.remove_book(book_id))
-    elif choice==3:
-        book_title=input("enter book title:")
-        new_library.search_book(book_title)
-    elif choice==4:
-        for book in new_library.generate_book():
-            book.display_book_details()
-    else:
-        print("invalid choice")
-    response=input("do you want to continue: y or n")
-    if response=="n":
-        stop=True
-#Create a user.
+def main():
+    print("library")
+    new_library=Library()
+    stop=False
+    while not stop:
+        choice = int(input("Enter your choice:1.add book 2.remove book 3.search book 4.display book."))
+        if choice==1:
+            print(new_library.add_book())
+        elif choice==2:
+            book_id = int(input("enter book id:"))
+            print(new_library.remove_book(book_id))
+        elif choice==3:
+            book_title=input("enter book title:")
+            new_library.search_book(book_title)
+        elif choice==4:
+            for book in new_library.generate_book():
+                book.display_book_details()
+        else:
+            print("invalid choice")
+        response=input("do you want to continue: y or n")
+        if response=="n":
+            stop=True
+    #Create a user.
 
-print("User")
-new_user=LibraryUser()
-stop=False
-while not stop:
-    choice = int(input("Enter your choice:1.borrow book 2.return book 3.track borrowed book"))
-    if choice==1:
-        book_id = int(input("enter book id:"))
-        print(new_user.borrow_book(new_library,book_id))
-    elif choice==2:
-        print(new_user.return_book(new_library))
-    elif choice==3:
-        new_user.track_borrowed_books()
-    else:
-        print("invalid choice")
-    response=input("do you want to continue: y or n")
-    if response=="n":
-        stop=True
+    print("User")
+    new_user=LibraryUser()
+    stop=False
+    while not stop:
+        choice = int(input("Enter your choice:1.borrow book 2.return book 3.track borrowed book"))
+        if choice==1:
+            book_id = int(input("enter book id:"))
+            print(new_user.borrow_book(new_library,book_id))
+        elif choice==2:
+            print(new_user.return_book(new_library))
+        elif choice==3:
+            new_user.track_borrowed_books()
+        else:
+            print("invalid choice")
+        response=input("do you want to continue: y or n")
+        if response=="n":
+            stop=True
+if __name__=="__main__":
+    main()
